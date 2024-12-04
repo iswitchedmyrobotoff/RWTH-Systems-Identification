6b6af2aff9cdbbf3d1d72d68b9bc6732adbe4bde
Montagsbrezel <moritz-blau@gmx.net>
2024-12-04 17:15:44 +0100

    Versuch V2 Dokumentation push

---
+++ P_IC/Core/Src/Enums.h
@@ -0,0 +1,18 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+typedef enum {
+    IDLE,
+    OPERATING,
+	SHOW_RESULTS,
+    CLEANING_UP
+} State;
+
+typedef enum {
+    RED,
+    GREEN,
+    BLUE,
+    BLINKING // UPDATE
+} LED;
+
+#endif
